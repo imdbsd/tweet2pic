2d151617c5694378bb0106ce51cf87e8c949a484
surya darma <budi.surya@kumparan.com>
2021-01-31 15:30:21 +0800

    setup configs

---
+++ tweetservice/src/configs.ts
@@ -0,0 +1,7 @@
+const configs = {
+  API_KEY_1: process.env.API_KEY_1 || '',
+  API_SECRET_KEY: process.env.API_SECRET_KEY || '',
+  API_BEARER_TOKEN: process.env.API_BEARER_TOKEN || '',
+}
+
+export default configs
--- tweetservice/src/server.ts
+++ tweetservice/src/server.ts
@@ -1,6 +1,11 @@
+import dotenv from 'dotenv'
+import path from 'path'
 import app from './app/app'
 
 const PORT = 4000
+dotenv.config({
+  path: path.resolve(__dirname, '../.env'),
+})
 
 app.listen(PORT, () => {
   console.log(`listenning on ${PORT}`)
